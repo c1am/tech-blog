c1e1dfa542e7521b7a976ec1ce2cad4eb6261092
Giyu <80783085+c1am@users.noreply.github.com>
2021-08-26 23:48:06 -0400

    edited file names

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const User = require('./User');
-const Post = require('./Post');
+const User = require('./usertest');
+const Post = require('./posttest');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
